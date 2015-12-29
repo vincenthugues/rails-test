0d986564a52a925ad0675cc2089a649251c06d00
Vincent Hugues <vincenthugues.dev@gmail.com>
2015-12-29 01:28:16 +0100

    Add Place validation

--- places/app/models/place.rb
+++ places/app/models/place.rb
@@ -1,2 +1,9 @@
 class Place < ActiveRecord::Base
+	validates :name, presence: true, length: { in: 3..30 }
+	validates :latitude, presence: true
+	validates :longitude, presence: true
+	validates :hashtags, allow_blank: true, format: {
+      :with => /\A(\w+([\s|,]\w)*)*\z/,
+      :message => "must contain only alphanumeric words separated by spaces or commas"
+    }
 end
